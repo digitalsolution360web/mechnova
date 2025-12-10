8e7cd099c0852eae61802f7fb60d929663ad81e7
digitalsolution360global <ds360host@gmail.com>
2025-12-10 22:40:34 +0530

    Update page.js

--- app/products/[slug]/page.js
+++ app/products/[slug]/page.js
@@ -247,7 +247,7 @@ const modelData = modelDataMap[locale] || modelDataEn;
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
               </svg>
-              {t('home.productsPage.backToProducts')}
+              {t('home.productsPage.backToProducts')} Arpita
             </Link>
           </div>
         </div>
